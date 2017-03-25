3aa2a7c696b3a2a6dc4189c1a2039d1e5febecda
drGreenthumb1A4 <filip.pilot@gmail.com>
2017-03-25 09:00:03 +0100

    3rd commit, almost done

--- client.js
+++ client.js
@@ -3,19 +3,24 @@ var exec = require('child_process').exec;
 var cmd = 'sh skripta.sh';
 
 var rjesenje;
-exec(cmd, function(error, stdout, stderr) {
-    console.log(stdout);
-    rjesenje = stdout;
-    rjesenje = rjesenje.slice(0, rjesenje.length - 1)
-    fetch('http://192.168.0.71:8080/listen', 
-    {
-        method: 'POST',
-        headers:  {'Content-Type': 'application/json'},
-        body: JSON.stringify({'temp': rjesenje}) })
-    .then(function(res) {
-        return res.json();
-    }).then(function(json) {
-        console.log(json);
+
+function eggzecute() {
+    exec(cmd, function (error, stdout, stderr) {
+        console.log(stdout);
+        rjesenje = stdout;
+        rjesenje = rjesenje.slice(0, rjesenje.length - 1)
+        fetch('http://192.168.1.12:8080/listen',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'temp': rjesenje })
+            })
+            .then(function (res) {
+                return res.json();
+            }).then(function (json) {
+                console.log(json);
+            })
     })
-})
+}
+setInterval(eggzecute, 5000);
 
